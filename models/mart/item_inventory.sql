f6b57daf73613c2114306bdf9ddc2a63b8dcde58
Dinesh <dineshgds@gmail.com>
2021-08-13 18:46:42 -0500

    Added product group model

--- models/mart/item_inventory.sql
+++ models/mart/item_inventory.sql
@@ -1,11 +1,11 @@
 with inv_result AS
 (
     SELECT 
-    --# Location properties 
+    --# Stock/Inventory properties 
         il.[location_id] 
         ,il.[inv_mast_uid] 
         ,i.[item_id]
-        ,i.[item_desc]
+        ,i.[item_desc] as [Item Description]
         ,il.[stockable] 
         ,il.[sellable] 
         ,il.[buy] 
@@ -20,8 +20,12 @@ with inv_result AS
         ,il.[qty_backordered] 
         ,il.[qty_in_transit]
         ,il.[product_group_id] 
+        ,pg.[product_group_desc] as [Product Group Description]
+        ,pg.[asset_account_no] as [Asset Account No]
     FROM {{ ref('stg_inv_loc')}} as il
     LEFT JOIN {{ref('stg_item_mast')}} as i on il.[inv_mast_uid] = i.[inv_mast_uid]
+    LEFT JOIN {{ref('stg_product_group')}} as pg on il.[product_group_id] = pg.[product_group_id]
+    WHERE i.[other_charge_item] <> 'Y'
 )
 
 select * from inv_result
